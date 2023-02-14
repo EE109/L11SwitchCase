053d4497c2dca824c473f5d74a6c841a480a82f2
Prof John David Zehnpfennig II, PE <111508888+prof-jz@users.noreply.github.com>
2023-02-14 18:02:01 +0000

    1. Reformatted the bizCard output. 2. Built out fully functional tipCalc. 3. Added screen clearing capabilities. 'system("clear")'. 4. Added program pausing feature via 'getchar()'.

--- .ccls-cache/@home@runner@L11SwitchCase/main.cpp
+++ .ccls-cache/@home@runner@L11SwitchCase/main.cpp
@@ -9,6 +9,9 @@
 *********************************************/
 #include <iostream>
 #include <string>
+#include <iomanip> //JZ - to manipulate cursor position in output
+#include <stdlib.h> //JZ - to clear the screen
+#include <stdio.h>
 
 using namespace std;
 
@@ -26,14 +29,15 @@ int main()
   while (selection < 0 || selection > 6)
     {
       //JZ - Output Main Menu 
-      cout << "\n\n1 - Pasta Cooking Tutorial";
-      cout << "\n\n2 - Fishing Tutorial";
-      cout << "\n\n3 - Tip Calculator";
-      cout << "\n\n4 - Business Card Prep";
-      cout << "\n\n5 - Book a Reservation";
-      cout << "\n\n6 - Address Book";
-      cout << "\n\n0 - Exit Program";
-      cout << "\n\nPlease make your selection (0-2): ";
+      system("clear"); //JZ - passess command to linux shell to clear the console
+      cout << "\n1 - Pasta Cooking Tutorial";
+      cout << "\n2 - Fishing Tutorial";
+      cout << "\n3 - Tip Calculator";
+      cout << "\n4 - Business Card Prep";
+      cout << "\n5 - Book a Reservation";
+      cout << "\n6 - Address Book";
+      cout << "\n0 - Exit Program";
+      cout << "\n\nPlease make your selection (0-6): ";
       //JZ - collect user input
       cin >> selection;
       //launch selected function or exit
@@ -60,7 +64,7 @@ int main()
           tipCalc();
           break;          
           }
-          case 3: //JZ - Tip Calculator
+        case 4: //JZ - Tip Calculator
           {
           bizCard();
           break;          
@@ -107,11 +111,12 @@ void bizCard()
   //JZ - Business Card Menu
   while (sel != 'q' && sel != 'Q')
     {
-      cout << "\n\n\t\t\tBusiness Card!";
-      cout << "\n\t\t\tD - Print Default Business Card";
+      system("clear"); //JZ - clears the screen
+      cout << "\t\t\t\tBusiness Card!";
+      cout << "\n\n\t\t\tD - Print Default Business Card";
       cout << "\n\t\t\tC - Print Custom Business Card";
       cout << "\n\t\t\tQ - Quit Printing Business Cards";
-      cout << "\nEnter your selection: ";
+      cout << "\n\nEnter your selection: ";
       cin >> sel;
       //JZ - input validations
       while (sel != 'D' && sel != 'd' && sel != 'C' && sel != 'c' && sel != 'Q' && sel != 'q' )
@@ -132,40 +137,109 @@ void bizCard()
         business = "Smoothie Shock!";
         phone = "+1-256-BAN-ANA1";
         address = "123 Main Street | Normal, Alabama"; 
-        email = "bigBanana@SmoothieShockAL!!.xyz";
+        email = "bigBanana@SmoothShock.xyz";
       }
-      else if (sel == 'C' || sel == 'c') //JZ - collect custom values
+      else if (sel == 'C' || sel == 'c') //JZ - collect custom values.  Using 'getline(cin, VAR) so the user can input ' ' space characters'
       {
         cout << "\nPlease enter the person's name: ";
-        cin >> name;
+        getline(cin, name);
         cout << "\nPlease enter the person's title: ";
-        cin >> title;
+        getline(cin,title);
         cout << "\nPlease enter the person's business: ";
-        cin >> business;
+        getline(cin, business);
         cout << "\nPlease enter the person's phone number: ";
-        cin >> phone;
+        getline(cin, phone);
         cout << "\nPlease enter the person's business email: ";
-        cin >> email;   
+        getline(cin, email);   
         cout << "\nPlease enter the person's business address: ";
-        cin >> address; 
+        getline(cin, address); 
       }
 
       //JZ - Output 2 x 3 array of business cards
       for (int x = 0 ; x < 3 ; x++)
         {
-          cout << "\n\n\n\t" << business << "\t\t\t\t" << name;
-          cout << "\\n\n" << name << "\t\t\t\t" << name;
-          cout << "\n" << title << "\t\t\t\t" << title;
-          cout << "\n" << phone << "\t\t\t\t" << phone;
-          cout << "\n" << email << "\t\t\t\t" << email;
-          cout << "\n\n" << address << "\t\t\t" << address;
+          cout << "\n\n\n\t\t" << left << setw(40) << business;
+          cout << left << setw(40) << business;
+          
+          cout << "\n\n\t" << left << setw(40) << name; 
+          cout << left << setw(40) << name;
+     
+          cout << "\n\t" << left << setw(40) << title;
+          cout << left << setw(40) << title;
+          
+          cout << "\n\n" << left << setw(40) << phone;
+          cout << left << setw(40) << phone;
+          
+          cout << "\n" << left << setw(40) << email;
+          cout << left << setw(40) << email;
+          
+          cout << "\n" << left << setw(40) << address;
+          cout << left << setw(40) << address;
         }
+      cout << "\n\nPress any key to return to the Business Card menu.";
+      getchar(); getchar();//JZ - causes the system to pause until user input.  Used double 'getchar()' to catch any stray characters in the buffer - so the user actually gets a chance to enter a keystroke.
      }
   
 }
 
 void tipCalc()
 {
-
+  //JZ - define and initialize variables
+  float bill = 0.0, tip = 0.0, total = 0.0, rate = -1;
+  char sel = 'a';
   
+  //JZ - Business Card Menu
+  while (sel != 'q' && sel != 'Q')
+    {
+      rate = -1; //JZ - Reset rate for loops
+      system("clear"); //JZ - passes command to Linux to clear the console
+      cout << "\t\t\t\tTip Calculator!";
+      cout << "\n\n\t\t\tD - Calculate 15% Tip";
+      cout << "\n\t\t\tC - Calculate Custom Tip";
+      cout << "\n\t\t\tQ - Quit Calculating Tips";
+      cout << "\n\nEnter your selection: ";
+      cin >> sel;
+      
+      //JZ - input validation
+      while (sel != 'D' && sel != 'd' && sel != 'C' && sel != 'c' && sel != 'Q' && sel != 'q' )
+        {
+          cout << "\nInvalid Input!  Please enter D, C, or Q: ";
+          cin >> sel;
+        }
+      
+           //JZ - set tiprate
+      if (sel == 'Q' || sel == 'q') //JZ - start with exit case 
+        {
+          cout << "\n\nThanks for using the Tip Calculator app!";
+          return;
+        }
+      else if (sel == 'd' || sel == 'D')
+        {
+          rate = 15;
+        }
+      else if (sel == 'c' || sel == 'C')
+        {
+          //JZ - Input validation verifies tip rate is between 0% and 100%
+          while (rate > 100 || rate < 0)
+            {
+              cout << "\nPlease enter the tip rate as a whole number.  For example, you would for an 18% tip you input: 18  Rate: ";
+              cin >> rate;              
+            }          
+        }
+      
+      
+      //JZ - collect bill amount
+      cout << "\n\nPlease enter the pre-tip bill value: $";
+      cin >> bill;
+     
+      //JZ - make calculations
+      tip = bill * (rate/100);
+      total = bill + tip;
+      //JZ - Output results
+      cout << "\n\nFor the $" << bill <<" bill, the tip is $" << tip << " at a rate of " << rate << "%.  This brings the total cost to $" << total << "  Press any key to return to the Tip Calculator menu.";
+      getchar(); getchar();//JZ - causes the system to pause until user input.  Used double 'getchar()' to catch any stray characters in the buffer - so the user actually gets a chance to enter a keystroke.
+      
+
+   }
 }
+  
